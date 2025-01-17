6185ac815a7bfe6cc9843d2ebae23a1809c36c16
Anuj-Kumar-Jain <jain.anuj1818@gmail.com>
2025-01-17 22:43:08 +0530

    user authentication done

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,14 @@ const express = require('express');
 const cors = require('cors');
 const connectToDb = require('./db/db.js');
 const userRoutes = require('./routes/user.routes.js')
+const cookieParser = require('cookie-parser')
 const app = express();
 
 connectToDb();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
+app.use(cookieParser());
 
 
 app.get('/' , (req , res) => {
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,12 +2,22 @@ const express = require('express');
 const router = express.Router();
 const {body} = require('express-validator');
 const userContoller= require('../controllers/user.controllers');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/register' , [
     body('email').isEmail().withMessage('Invalid Email'),
     body('fullname.firstname').isLength({min:3}).withMessage('First letter must be at least 3 characters'),
     body('password').isLength({min : 8}).withMessage('Password must be 8 characters long'),
 ] , userContoller.registerUser)
+
+router.post('/login' , [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min : 8}).withMessage("Password must be 8 characters long")
+] , userContoller.loginUser)
+
+router.get('/profile' , authMiddleware.authUser , userContoller.getUserProfile);
+
+router.get('/logout' , authMiddleware.authUser , userContoller.logoutUser)
 
 
 module.exports = router;
